6eff8a563763cb8168418b582ab1fa0486ed8f3a
SkynerSany <skynerstudent@gmail.com>
2023-02-11 16:06:35 +0300

    feat: add routes for groups

--- check-responses.js
+++ check-responses.js
@@ -1,5 +1,44 @@
 
-fetch('http://localhost:3333/users/3', {
+// fetch('http://localhost:3333/users/3', {
+//   method: 'DELETE'
+// }).then((res) => res.json()).then((res) => console.log(res))
+// fetch('http://localhost:3333/users/1').then((res) => res.json()).then((res) => console.log(res));
+
+// fetch('http://localhost:3333/users/2', {
+//   method: 'PUT',
+//   headers: {
+//     'Content-Type': 'application/json;charset=utf-8'
+//   },
+//   body: JSON.stringify({
+//     name: 'hgdfhdh Pet',
+//     balance: 15846.2,
+//     groups: JSON.stringify([1, 2])
+//   })
+// }).then((res) => res.json()).then((res) => console.log(res))
+
+// fetch('http://localhost:3333/groups', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json;charset=utf-8'
+//   },
+//   body: JSON.stringify({
+//     name: 'first',
+//     photo: 'https://photo.jpg',
+//     users: JSON.stringify([4, 5, 6]),
+//     expenses: JSON.stringify([
+//       {
+//         title: 'fgg', // название платежа (необязательное поле)
+//         amount: 5500, // сумма платежа
+//         by: 1, // id того кто купил
+//         for: [2, 3], // перечень id для кого купили
+//         date: 155225 // дата платежа
+//       }
+//     ])
+//   })
+// }).then((res) => res.json()).then((res) => console.log(res))
+
+fetch('http://localhost:3333/groups/5', {
   method: 'DELETE'
 }).then((res) => res.json()).then((res) => console.log(res))
-fetch('http://localhost:3333/users').then((res) => res.json()).then((res) => console.log(res));
+
+// fetch('http://localhost:3333/groups/2').then((res) => res.json()).then((res) => console.log(res));
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 const pool = require('../data/config');
+const user = require('../scripts/user');
 
 const router = (app) => {
   app.get('/', (request, response) => {
@@ -25,8 +26,8 @@ const router = (app) => {
 
   app.post('/users', (request, response) => {
     pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
-        if (error) throw error;
-        response.status(201).send(`User added with ID: ${result.insertId}`);
+      if (error) throw error;
+      response.status(201).send(`User added with ID: ${result.insertId}`);
     });
   });
 
@@ -43,6 +44,54 @@ const router = (app) => {
     pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
         if (error) throw error;
         response.send('User deleted.');
+    });
+  });
+
+  app.get('/groups/:id', (request, response) => {
+    const id = request.params.id;
+
+    pool.query('SELECT * FROM groups WHERE id = ?', id, (error, result) => {
+      if (error) throw error;
+      response.send(result);
+    });
+  });
+
+  app.get('/groups', (request, response) => {
+    const id = request.params.id;
+
+    pool.query('SELECT * FROM groups', (error, result) => {
+      if (error) throw error;
+      response.send(result);
+    });
+  });
+
+  app.post('/groups', (request, response) => {
+    pool.query('INSERT INTO groups SET ?', request.body, (error, result) => {
+        if (error) throw error;
+        response.status(201).send(`Group added with ID: ${result.insertId}`);
+    });
+  });
+
+  app.put('/groups/:id', (request, response) => {
+    const id = request.params.id;
+    pool.query('UPDATE groups SET ? WHERE id = ?', [request.body, id], (error, result) => {
+        if (error) throw error;
+        response.send('Group updated successfully.');
+    });
+  });
+
+  app.delete('/groups/:id', (request, response) => {
+    const id = request.params.id;
+    pool.query('SELECT * FROM groups WHERE id = ?', id, (error, group) => {
+      if (error) throw error;
+
+      const users = JSON.parse(group[0].users);
+      users.forEach((userId) => user.removeGroupFromUser(userId, id));
+    });
+
+    pool.query('DELETE FROM groups WHERE id = ?', id, (error, result) => {
+        if (error) throw error;
+        response.send('Group deleted.');
     });
   });
 }
