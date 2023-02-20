e2e5418eb88eab7ddfb83aac4865f3659ff582b9
Yury Andrabaila <Androbailo@gmail.com>
2023-02-21 00:01:00 +0200

    feat: add currencies page

--- src/layouts/createCurrenciesMain.ts
+++ src/layouts/createCurrenciesMain.ts
@@ -1,10 +1,18 @@
 import getHtmlElement from '../components/getHtmlElement';
 import currenciesArr from '../data/currencies';
+import setCurrency from '../features/setCurrency';
 
 function createCurrenciesMain() {
     document.querySelector('.main')?.remove();
+    document.querySelector('.shadow_active')?.classList.remove('shadow_active');
 
-    getHtmlElement({ parent: 'body', tag: 'main', style: ['main', 'main_currencies-page'] });
+    let currency = '';
+    if (localStorage.getItem('currency')) {
+        currency = <string>localStorage.getItem('currency');
+    }
+
+    const main = getHtmlElement({ parent: 'body', tag: 'main', style: ['main', 'main_currencies-page'] });
+    main.addEventListener('click', setCurrency);
     getHtmlElement({ parent: '.main', style: ['main__wrapper', 'main__wrapper_currencies-page'] });
     getHtmlElement({ parent: '.main__wrapper', style: ['main__header'] });
     getHtmlElement({
@@ -15,40 +23,51 @@ function createCurrenciesMain() {
         window.history.back();
     });
 
-    getHtmlElement({ parent: '.main__wrapper', tag: 'ul', style: ['currencies__item'] });
+    let currencyName = '';
+    currenciesArr.forEach((currencyObj) => {
+        if (currencyObj.Cur_Abbreviation === currency) {
+            currencyName = <string>currencyObj.Cur_Name_Eng;
+        }
+    });
 
+    const selectedItem = getHtmlElement({ parent: '.main__wrapper', tag: 'ul', style: ['currencies__item'] });
+    selectedItem.id = currency;
+    selectedItem.addEventListener('click', () => window.history.back());
     getHtmlElement({
         parent: '.currencies__item',
         tag: 'span',
         style: ['currencies__abbreviation'],
-        content: 'BYN',
+        content: currency,
     });
     getHtmlElement({
         parent: '.currencies__item',
         tag: 'span',
         style: ['currencies__currency-name'],
-        content: 'Belarusian Ruble',
+        content: currencyName,
     });
 
     getHtmlElement({ parent: '.main__wrapper', tag: 'ul', style: ['currencies'] });
     currenciesArr.forEach((currencyObj) => {
-        getHtmlElement({
-            parent: '.currencies',
-            tag: 'li',
-            style: ['currencies__item'],
-        });
-        getHtmlElement({
-            parent: '.currencies__item',
-            tag: 'span',
-            style: ['currencies__abbreviation'],
-            content: <string>currencyObj.Cur_Abbreviation,
-        });
-        getHtmlElement({
-            parent: '.currencies__item',
-            tag: 'span',
-            style: ['currencies__currency-name'],
-            content: <string>currencyObj.Cur_Name_Eng,
-        });
+        if (currencyObj.Cur_Abbreviation !== currency) {
+            getHtmlElement({
+                parent: '.currencies',
+                tag: 'li',
+                style: ['currencies__item'],
+            }).id = <string>currencyObj.Cur_Abbreviation;
+
+            getHtmlElement({
+                parent: '.currencies__item',
+                tag: 'span',
+                style: ['currencies__abbreviation'],
+                content: <string>currencyObj.Cur_Abbreviation,
+            }).id = <string>currencyObj.Cur_Abbreviation;
+            getHtmlElement({
+                parent: '.currencies__item',
+                tag: 'span',
+                style: ['currencies__currency-name'],
+                content: <string>currencyObj.Cur_Name_Eng,
+            }).id = <string>currencyObj.Cur_Abbreviation;
+        }
     });
 }
 export default createCurrenciesMain;
